a397248c8920e839dd6abbd7cbd948f01929ea2f
Shadi Shahi <71129585+shadishahi@users.noreply.github.com>
2021-08-26 15:10:23 +0200

    Create time_to_second.py

---
+++ time_to_second.py
@@ -0,0 +1,6 @@
+hour= int(input("Please Enter hour: ")) 
+minute =int(input("Please Enter minute: "))
+seconds =int(input("Please Enter second: "))
+
+total_seconds =((hour*3600)+(minute*60)+seconds)
+print(seconds)
